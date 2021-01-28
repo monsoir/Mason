b1c037759bdf57125aebb14a00e1dede2dbed5b3
Monsoir <raeyeung.mon@gmail.com>
2021-01-28 23:53:11 +0800

    feat: init

---
+++ Mason/Mason.h
@@ -0,0 +1,18 @@
+//
+//  Mason.h
+//  Mason
+//
+//  Created by monsoir on 1/28/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mason.
+FOUNDATION_EXPORT double MasonVersionNumber;
+
+//! Project version string for Mason.
+FOUNDATION_EXPORT const unsigned char MasonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mason/PublicHeader.h>
+
+
